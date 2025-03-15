28a8de69072c614c56634e4a686249c5ca033bd4
Eudes-Dev <euudesdev@gmail.com>
2025-03-15 01:22:37 +0000

    Home Layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ const bebasNeue = localFont({
   src: [
     { path: '/fonts/BebasNeue-Regular.ttf', weight: '400', style: 'normal' },
   ],
-  variable: '--babas-neue'
+  variable: '--bebas-neue'
 });
 
 export const metadata: Metadata = {
